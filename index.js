4998737eba26420cf381cbd965d89753ca5f851d
Vadim Sieviertsev <vadikpointvadik@gmail.com>
2019-06-19 21:34:42 +0300

    m

--- index.js
+++ index.js
@@ -2,43 +2,32 @@ const Telegraf = require('telegraf');
 const config = require('./config.json');
 const parser = require('./currencyRateParser.js')
 const request = require('request');
-const requestClosed = false;
-const bot = new Telegraf(config.token);
-bot.telegram.setWebhook(config.webhook);
+const bot = new Telegraf(config.token, {webhookReply : true});
+
 bot.hears(/\/start/, (ctx) => {
     return ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then(() => {
-        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`).then(() => {
-            requestClosed = true;
-        });
+        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`);
     });
 });
 
 bot.hears(/\/help/, (ctx) => {
     return ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then(() => {
-        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`).then(() => {
-            requestClosed = true;
-        });
+        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`);
     });
 });
 
 bot.on('sticker', (ctx) => {
-    return ctx.reply('Nice one!').then(() => { return ctx.reply('Really nice') }).then(() => {
-        requestClosed = true;
-    });;
+    return ctx.reply('Nice one!').then(() => { return ctx.reply('Really nice') });
 });
 
 bot.hears(/^\/rate [a-zA-Z]{3}$/, (ctx) => {
     var currencyLetterCode = ctx.match.input.split(' ')[1].toUpperCase();
     parser(currencyLetterCode, null,
         (result) => {
-            ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id }).then(() => {
-                requestClosed = true;
-            });
+            ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
         },
         (error) => {
-            ctx.reply(error, { reply_to_message_id: ctx.message.message_id }).then(() => {
-                requestClosed = true;
-            });;
+            ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
         }
     );
 });
@@ -50,14 +39,10 @@ bot.hears(/^\/rate [a-zA-Z]{3}\-[a-zA-Z]{3}$/, (ctx) => {
     return ctx.reply(`Looking for ${currencyLetterCode} exchange rate to ${currencyCompare}`).then(() => {
         parser(currencyLetterCode, currencyCompare,
             (result) => {
-                ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id }).then(() => {
-                    requestClosed = true;
-                });;
+                ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
             },
             (error) => {
-                ctx.reply(error, { reply_to_message_id: ctx.message.message_id }).then(() => {
-                    requestClosed = true;
-                });;
+                ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
             }
         );
     });
@@ -80,11 +65,9 @@ const setupAnswer = (currencyData) => {
 module.exports = (req, res) => {
     if (req.body) {
         bot.handleUpdate(req.body, res).then(() => {
-            while (true) {
-                if (requestClosed) {
-                    res.end(200, 'OK');
-                }
-            }
+           setTimeout(()=>{
+            res.end(200, 'OK');
+           }, 4000);
         });
     }
     else {
