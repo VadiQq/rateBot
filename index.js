03f5dde439b838eae4d573f182d2ed5c60f62efe
Vadim Sieviertsev <vadikpointvadik@gmail.com>
2019-06-19 17:58:35 +0300

    m

--- index.js
+++ index.js
@@ -2,46 +2,48 @@ const Telegraf = require('telegraf');
 const config = require('./config.json');
 const parser = require('./currencyRateParser.js')
 
-const bot = new Telegraf(config.token);
+const bot = new Telegraf(config.token, { webhookReply: false });
 
-bot.hears(/\/start/, (ctx) => ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then((msg) => {
-    return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`)
-}));
-
-bot.hears(/\/help/, (ctx) => ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then(() => {
-    return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`)
-}));
-
-bot.on('sticker', (ctx) => ctx.reply('Nice one!').then(()=> { return ctx.reply('Really nice')}));
-
-bot.hears(/^\/rate [a-zA-Z]{3}$/, (ctx) => {    
-    var currencyLetterCode = ctx.match.input.split(' ')[1].toUpperCase();
-    ctx.reply(`Looking for ${currencyLetterCode} to UAH exchange rate`).then(() => {
-        parser(currencyLetterCode, null,
-            (result) => {
-                ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
-            },
-            (error) => {
-                ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
-            }
-        );
+bot.hears(/\/start/, (ctx) => {
+    return ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then(() => {
+        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`)
     });
+});
+
+bot.hears(/\/help/, (ctx) => {
+    return ctx.reply(`Type /rate CUR (currency literal code) to get daily exchange rate to UAH`).then(() => {
+        return ctx.reply(`Type /rate CUR-CMP (currency literal code)-(compare currency code) to get daily exchange rate to specific currency`)
+    });
+});
+
+bot.on('sticker', (ctx) => {
+    return ctx.reply('Nice one!').then(() => { return ctx.reply('Really nice') });
+});
+
+bot.hears(/^\/rate [a-zA-Z]{3}$/, (ctx) => {
+    var currencyLetterCode = ctx.match.input.split(' ')[1].toUpperCase();
+    parser(currencyLetterCode, null,
+        (result) => {
+            ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
+        },
+        (error) => {
+            ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
+        }
+    );
 });
 
 bot.hears(/^\/rate [a-zA-Z]{3}\-[a-zA-Z]{3}$/, (ctx) => {
     var requestData = ctx.match.input.split(' ')[1].split('-');
     var currencyLetterCode = requestData[0].toUpperCase();
     var currencyCompare = requestData[1].toUpperCase();
-    ctx.reply(`Looking for ${currencyLetterCode} to ${currencyCompare} exchange rate`).then(() => {
-        parser(currencyLetterCode, currencyCompare,
-            (result) => {
-                ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
-            },
-            (error) => {
-                ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
-            }
-        );
-    });
+    parser(currencyLetterCode, currencyCompare,
+        (result) => {
+            ctx.reply(setupAnswer(result), { parse_mode: 'HTML', disable_web_page_preview: true, reply_to_message_id: ctx.message.message_id });
+        },
+        (error) => {
+            ctx.reply(error, { reply_to_message_id: ctx.message.message_id });
+        }
+    );
 });
 
 bot.on('message', (ctx) => {
